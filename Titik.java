add2c4c4bd1736898c6c7af6075d4fab5a51cc80
Aditya Haidar Faishal <ahf.aditya@gmail.com>
2024-02-21 11:58:42 +0700

    Pertemuan 1 Prak PBO

---
+++ Titik.java
@@ -0,0 +1,36 @@
+// File        : Titik.java          21/02/24
+// Penulis     : Aditya Haidar Faishal
+// Deskripsi   : Kelas Titik yang merupakan sebuah titik dengan absis dan ordinat
+
+public class Titik{
+    double absis;
+    double ordinat;
+    static int counterTitik;
+
+
+    Titik(){
+        counterTitik++;
+        absis = 0;
+        ordinat = 0;
+    }
+
+    void setAbsis(double a){
+        absis = a;
+    }
+
+    void setOrdinat(double o){
+        ordinat = o;
+    }
+
+    double getAbsis(){
+        return absis;
+    }
+
+    double getOrdinat(){
+        return ordinat;
+    }
+
+    static int getCounterTitik(){
+        return counterTitik;
+    }
+}
